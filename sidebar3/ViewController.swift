ea1d79877cc50617151860a65067417543359de1
kirtanBaldha <kirtan9397@gmail.com>
2023-07-13 12:57:26 +0530

    u8iol9i

--- sidebar3/ViewController.swift
+++ sidebar3/ViewController.swift
@@ -4,16 +4,21 @@
 //
 //  Created by R88 on 12/07/23.
 //
-
 import UIKit
-
+import iOSDropDown
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var text1: DropDown!
+    @IBOutlet weak var text2: DropDown!
+    @IBOutlet weak var text3: DropDown!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        text1.optionArray = ["true","false"]
+        text1.text = "check"
+        text2.optionArray = ["mahesana","surat","vadodra","bharuch","gandhinagar","rajkot","bhavnagar","ahemdabad"]
+        text2.text = "city"
+        text3.optionArray = ["1","2","3","4","5"]
+        
+        
     }
-
-
 }
-
---
+++ sidebar3/ViewController1.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController1.swift
+//  sidebar3
+//
+//  Created by R88 on 12/07/23.
+//
+import UIKit
+class ViewController1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func submit(_ sender: Any) {
+    }
+}
